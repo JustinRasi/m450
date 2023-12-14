99259f693cad8b6359a4f118afb20d6298363cc1
justin <justin.rasi@edu.tbz.ch>
2023-12-14 15:12:33 +0100

    ding dong

--- automation-testing/spring-boot-angular-basic-lw2/src/main/js/my-app/protractor.conf.js
+++ automation-testing/spring-boot-angular-basic-lw2/src/main/js/my-app/protractor.conf.js
@@ -2,6 +2,9 @@ exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['./e2e/**/*.e2e-spec.ts'],
+  plugins: [
+    'karma-jasmine'
+  ],
   capabilities: {
     'browserName': 'chrome'
   }
--- automation-testing/spring-boot-angular-basic-lw2/src/main/js/my-app/src/app/e2e_tests/student-form.spec.ts
+++ automation-testing/spring-boot-angular-basic-lw2/src/main/js/my-app/src/app/e2e_tests/student-form.spec.ts
@@ -1,8 +1,15 @@
-import { browser } from 'protractor';
+import {browser, by, element} from "protractor";
 
-describe('Student Form', () => {
-  it('should submit the form successfully', () => {
-    browser.get('http://localhost:4200/student-form');
-    // Perform actions and assertions here
+describe('Student Form Test', function () {
+  it('should submit the form successfully', function () {
+    browser.get('students'); // Pfad zu Ihrer Studentenformularseite
+
+    // Füllen Sie das Formular aus
+    element(by.id('name')).sendKeys('John Doe');
+    element(by.id('email')).sendKeys('john.doe@example.com');
+    element(by.css('button.btn-info')).click();
+
+    // Überprüfen Sie, ob die Studentenliste angezeigt wird
+    expect(element.all(by.css('tr')).count()).toBeGreaterThan(0);
   });
 });
